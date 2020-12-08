ded8af4fedc22270fa8a459e31336082fe7e4882
rohar.anil <anil.rohartech@gmail.com>
2020-12-07 22:51:39 -0800

    first commit

---
+++ View/Header.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+	<link rel="stylesheet" href="css/custom.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+	<!-- SLIDER REVOLUTION 5.x CSS SETTINGS -->
+	<link rel="stylesheet" type="text/css" href="include/rs-plugin/css/settings.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="include/rs-plugin/css/layers.css">
+	<link rel="stylesheet" type="text/css" href="include/rs-plugin/css/navigation.css">
+	<!-- Document Title
+	============================================= -->
+	<title>Home - Shop | AshuDiHatti</title>
+
+	<style>
+
+		.revo-slider-emphasis-text {
+			font-size: 58px;
+			font-weight: 700;
+			letter-spacing: 1px;
+			font-family: 'Poppins', sans-serif;
+			padding: 15px 20px;
+			border-top: 2px solid #FFF;
+			border-bottom: 2px solid #FFF;
+		}
+
+		.revo-slider-desc-text {
+			font-size: 20px;
+			font-family: 'Lato', sans-serif;
+			width: 650px;
+			text-align: center;
+			line-height: 1.5;
+		}
+
+		.revo-slider-caps-text {
+			font-size: 16px;
+			font-weight: 400;
+			letter-spacing: 3px;
+			font-family: 'Poppins', sans-serif;
+		}
+
+		.tp-video-play-button { display: none !important; }
+
+		.tp-caption { white-space: nowrap; }
+	</style>
+</head>
+<body>
+			<!-- Top Bar
+				============================================= -->
+				<div id="top-bar">
+					<div class="container">
+						<div class="row justify-content-between align-items-center">
+							<div class="col-12 col-md-auto">
+								<p class="mb-0 py-2 text-center text-md-left"> Indian Wedding, Ethnic, Sangeet Wear, Salwar Suit, Traditional Dresses & Gowns</p>
+							</div>
+
+							<div class="col-12 col-md-auto pl-0">
+								<ul id="top-social">
+									<li><a href="https://m.facebook.com/Ashu-Di-Hatti-1260332320731253/" class="si-facebook"><span class="ts-icon"><i class="icon-facebook"></i></span><span class="ts-text">Facebook</span></a></li>
+									<li><a href="https://mobile.twitter.com/HattiAshu" class="si-twitter"><span class="ts-icon"><i class="icon-twitter"></i></span><span class="ts-text">Twitter</span></a></li>
+									<li><a href="https://instagram.com/ashudihattiphagwara?igshid=30ubnmsrieg" class="si-instagram"><span class="ts-icon"><i class="icon-instagram2"></i></span><span class="ts-text">Instagram</span></a></li>
+									<li><a href="tel:098720 14014" class="si-call"><span class="ts-icon"><i class="icon-call"></i></span><span class="ts-text">09872014014</span></a></li>
+									<li><a href="mailto:info@ashudihatti.com" class="si-email3"><span class="ts-icon"><i class="icon-envelope-alt"></i></span><span class="ts-text">info@ashudihatti.com</span></a></li>
+								</ul><!-- #top-social end -->
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- #top-bar end -->
+					<!-- #Header start-->
+					<header id="header">
+						<div id="header-wrap">
+							<div class="container">
+								<div class="header-row">
+
+									   <!-- Logo
+			                    ============================================= -->
+			                    <div id="logo" class="ml-auto ml-md-0">
+			                        <a href="index.html" class="standard-logo" data-dark-logo="images/aashuimage/logo1.png"><img class="mx-auto" src="images/aashuimage/logo1.png" alt=""></a>
+			                        <a href="index.html" class="retina-logo" data-dark-logo="images/aashuimage/logo1.png"><img class="mx-auto" src="images/aashuimage/logo1.png" alt="Canvas Logo"></a>
+			                    </div><!-- #logo end -->
+
+									<div class="header-misc">
+
+										<!-- Top Search
+										============================================= -->
+										<div id="top-search" class="header-misc-icon">
+											<a href="#" id="top-search-trigger"><i class="icon-line-search"></i></a>
+										</div><!-- #top-search end -->
+
+										<!-- Top Cart
+										============================================= -->
+										<div id="top-cart" class="header-misc-icon  d-sm-block">
+											<a href="#" id="top-cart-trigger"><i class="icon-user-circle"></i></a>						
+										</div><!-- #top-cart end -->
+
+									</div>
+
+									<div id="primary-menu-trigger">
+										<svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
+									</div>
+
+									<!-- Primary Navigation
+									============================================= -->
+									<nav class="primary-menu">
+
+										<ul class="menu-container">
+											<!-- Men Section Of navcigation bar -->
+											<li class="menu-item "><a class="menu-link" href="#"><div>Men</div></a>
+												<ul class="sub-menu-container">
+													<li class="menu-item"><a class="menu-link" href="index-corporate.html"><div>Home - Corporate</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-corporate.html"><div>Corporate - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-2.html"><div>Corporate - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-3.html"><div>Corporate - Layout 3</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-4.html"><div>Corporate - Layout 4</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-portfolio.html"><div>Home - Portfolio</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-portfolio.html"><div>Portfolio - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-portfolio-2.html"><div>Portfolio - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-portfolio-3.html"><div>Portfolio - Masonry</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-portfolio-4.html"><div>Portfolio - AJAX</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-blog.html"><div>Home - Blog</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-blog.html"><div>Blog - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-blog-2.html"><div>Blog - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-blog-3.html"><div>Blog - Layout 3</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-shop.html"><div>Home - Shop</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-shop.html"><div>Shop - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-shop-2.html"><div>Shop - Layout 2</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-magazine.html"><div>Home - Magazine</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-magazine.html"><div>Magazine - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-magazine-2.html"><div>Magazine - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-magazine-3.html"><div>Magazine - Layout 3</div></a></li>
+														</ul>
+													</li>									
+												</ul>
+											</li>
+											<!--End Men Section Of navcigation bar -->
+										<!-- 	Women section of navigation bar -->
+											<li class="menu-item"><a class="menu-link" href="#"><div>Women</div></a>
+												<ul class="sub-menu-container">
+													<li class="menu-item"><a class="menu-link" href="index-corporate.html"><div>Home - Corporate</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-corporate.html"><div>Corporate - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-2.html"><div>Corporate - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-3.html"><div>Corporate - Layout 3</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-4.html"><div>Corporate - Layout 4</div></a></li>
+														</ul>
+													</li>									
+													<li class="menu-item"><a class="menu-link" href="index-magazine.html"><div>Home - Magazine</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-magazine.html"><div>Magazine - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-magazine-2.html"><div>Magazine - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-magazine-3.html"><div>Magazine - Layout 3</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="landing.html"><div>Home - Landing Page</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="landing.html"><div>Landing Page - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-2.html"><div>Landing Page - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-3.html"><div>Landing Page - Layout 3</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-4.html"><div>Landing Page - Layout 4</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-5.html"><div>Landing Page - Layout 5</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-fullscreen-image.html"><div>Home - Full Screen</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-fullscreen-image.html"><div>Full Screen - Image</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-fullscreen-slider.html"><div>Full Screen - Slider</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-fullscreen-video.html"><div>Full Screen - Video</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-onepage.html"><div>Home - One Page</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-onepage.html"><div>One Page - Default</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-onepage-2.html"><div>One Page - Submenu</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-onepage-3.html"><div>One Page - Dots Style</div></a></li>
+														</ul>
+													</li>										
+												</ul>
+											</li>
+											<!-- End Women section of navigation bar -->
+											<!-- 	Women section of navigation bar -->
+											<li class="menu-item "><a class="menu-link" href="#"><div>Handloom</div></a>
+												<ul class="sub-menu-container">
+													<li class="menu-item"><a class="menu-link" href="index-corporate.html"><div>Home - Corporate</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-corporate.html"><div>Corporate - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-2.html"><div>Corporate - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-3.html"><div>Corporate - Layout 3</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-corporate-4.html"><div>Corporate - Layout 4</div></a></li>
+														</ul>
+													</li>									
+													<li class="menu-item"><a class="menu-link" href="index-magazine.html"><div>Home - Magazine</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-magazine.html"><div>Magazine - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-magazine-2.html"><div>Magazine - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-magazine-3.html"><div>Magazine - Layout 3</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="landing.html"><div>Home - Landing Page</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="landing.html"><div>Landing Page - Layout 1</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-2.html"><div>Landing Page - Layout 2</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-3.html"><div>Landing Page - Layout 3</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-4.html"><div>Landing Page - Layout 4</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="landing-5.html"><div>Landing Page - Layout 5</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-fullscreen-image.html"><div>Home - Full Screen</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-fullscreen-image.html"><div>Full Screen - Image</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-fullscreen-slider.html"><div>Full Screen - Slider</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-fullscreen-video.html"><div>Full Screen - Video</div></a></li>
+														</ul>
+													</li>
+													<li class="menu-item"><a class="menu-link" href="index-onepage.html"><div>Home - One Page</div></a>
+														<ul class="sub-menu-container">
+															<li class="menu-item"><a class="menu-link" href="index-onepage.html"><div>One Page - Default</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-onepage-2.html"><div>One Page - Submenu</div></a></li>
+															<li class="menu-item"><a class="menu-link" href="index-onepage-3.html"><div>One Page - Dots Style</div></a></li>
+														</ul>
+													</li>										
+												</ul>
+											</li>
+											<!-- End Women section of navigation bar -->
+											<li class="menu-item"><a class="menu-link" href="#"><div>About</div></a></li>
+											<li class="menu-item"><a class="menu-link" href="#"><div>FB Live</div></a></li>	
+
+
+										</ul>
+									</nav><!-- #primary-menu end -->
+
+									<form class="top-search-form" action="search.html" method="get">
+										<input type="text" name="q" class="form-control" value="" placeholder="search here.." autocomplete="off">
+									</form>
+
+								</div>
+							</div>
+						</div>
+						<div class="header-wrap-clone"></div>
+					</header>
+					<!-- #header end -->
+</body>
+</html>
---
+++ View/footer.php
@@ -0,0 +1,142 @@
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+	<link rel="stylesheet" href="css/custom.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!-- SLIDER REVOLUTION 5.x CSS SETTINGS -->
+	<link rel="stylesheet" type="text/css" href="include/rs-plugin/css/settings.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="include/rs-plugin/css/layers.css">
+	<link rel="stylesheet" type="text/css" href="include/rs-plugin/css/navigation.css">
+	<!-- Document Title
+	============================================= -->
+	<title>Home - Shop | AshuDiHatti</title>
+
+</head>
+<body>
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="bg-color dark border-0">
+			<div class="container-fluid">
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap pb-5">
+					<div class="row">
+						<div class="col-lg-2 col-md-3 col-6">
+							<div class="widget">
+								<h4 class="ls0 mb-3 nott">Features</h4>
+								<ul class="list-unstyled iconlist ml-0">
+									<li><a href="#">Help Center</a></li>
+									<li><a href="#">Paid with Moblie</a></li>
+									<li><a href="#">Status</a></li>
+									<li><a href="#">Changelog</a></li>
+									<li><a href="#">Contact Support</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-3 col-6">
+							<div class="widget">
+								<h4 class="ls0 mb-3 nott">Support</h4>
+								<ul class="list-unstyled iconlist ml-0">
+									<li><a href="#">Home</a></li>
+									<li><a href="#">About</a></li>
+									<li><a href="#">FAQs</a></li>
+									<li><a href="#">Support</a></li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-3 col-6">
+							<div class="widget">
+								<h4 class="ls0 mb-3 nott">Trending</h4>
+								<ul class="list-unstyled iconlist ml-0">
+									<li><a href="#">Shop</a></li>
+									<li><a href="#">Portfolio</a></li>
+									<li><a href="#">Blog</a></li>
+									<li><a href="#">Events</a></li>
+									<li><a href="#">Forums</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-3 col-6">
+							<div class="widget">
+								<h4 class="ls0 mb-3 nott">Get to Know us</h4>
+								<ul class="list-unstyled iconlist ml-0">
+									<li><a href="#">Corporate</a></li>
+									<li><a href="#">Agency</a></li>
+									<li><a href="#">eCommerce</a></li>
+									<li><a href="#">Personal</a></li>
+									<li><a href="#">OnePage</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="widget">
+								<h4 class="ls0 mb-3 nott">Instagram Feed</h4>
+								<div id="instagram-photos" class="instagram-photos masonry-thumbs grid-5" data-user="blog.canvastemplate" data-count="10"></div>
+							</div>
+						</div>
+					</div>
+				</div><!-- .footer-widgets-wrap end -->
+			</div>
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container-fluid clearfix">
+
+					<div class="row justify-content-between align-items-center">
+						<div class="col-md-6">
+							Copyrights &copy; 2014 All Rights Reserved by Rohar Technology Inc.<br>
+							<div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
+						</div>
+
+						<div class="col-md-6 d-md-flex flex-md-column align-items-md-end mt-4 mt-md-0">
+							<ul class="list-unstyled d-flex flex-row mb-2 clearfix">
+								<li class="mr-2"><img src="demos/xmas/images/cards/visa.svg" alt="Visa" width="34"></li>
+								<li class="mr-2"><img src="demos/xmas/images/cards/mc.svg" alt="Master Card" width="34"></li>
+								<li class="mr-2"><img src="demos/xmas/images/cards/ae.svg" alt="American Express" width="34"></li>
+								<li class="mr-2"><img src="demos/xmas/images/cards/pp.svg" alt="PayPal" width="34"></li>
+								<li class="mr-2"><img src="demos/xmas/images/cards/sk.svg" alt="Skrill" width="34"></li>
+								<li class="mr-2"><img src="demos/xmas/images/cards/2co.svg" alt="2CheckOut" width="34"></li>
+								<li class="mr-2"><img src="demos/xmas/images/cards/wu.svg" alt="Western Union" width="34"></li>
+							</ul>
+							<div class="copyrights-menu copyright-links clearfix">
+								<a href="#">About</a>/<a href="#">Features</a>/<a href="#">FAQs</a>/<a href="#">Contact</a>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+		</footer><!-- #footer end -->
+</div><!-- #wrapper end -->
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+	<!-- JavaScripts
+	============================================= -->
+	<script src="js/jquery.js"></script>
+	<script src="js/plugins.min.js"></script>
+	<!-- Footer Scripts
+	============================================= -->
+	<script src="js/functions.js"></script>
+	
+	
+</body>
+</html>
